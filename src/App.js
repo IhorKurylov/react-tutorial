e3771c1518aee6dad4aac68157ad910f68d1f452
IhorKurylov <idSNrY7I3xhB>
2023-07-03 19:12:50 +0300

    lesson4-5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import UsersComponent from './components/UsersComponent/UsersComponent.jsx';
+import { createContext, useState, useEffect } from 'react';
+import UserPosts from './components/UserPosts/UserPosts.jsx';
+import LoginPage from './pages/LoginPage/LoginPage.jsx';
 
-function App() {
+const title = 'Users list';
+export const UsersContext = createContext(null);
+
+export const AppContext = createContext(null);
+
+const App = () => {
+  const [ show, setShow ] = useState(true);
+  const [ users, setUsers ] = useState([]);
+
+
+  const info = "Company name";
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    try {
+      let response = await fetch('https://jsonplaceholder.typicode.com/users');
+      let data = await response.json();
+      setUsers(data);
+    } catch {
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className="link">{title}</h1>
+      <div style={{display: 'flex', justifyContent: 'space-around', width: '100%'}}>
+        {/*<AppContext.Provider value={{info, users, setUsers}}>*/}
+        {/*  <UsersComponent />*/}
+        {/*  <UserPosts />*/}
+        {/*</AppContext.Provider>*/}
+        <LoginPage />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
